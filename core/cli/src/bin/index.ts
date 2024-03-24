8dcb3618230a453e0e0af02953eee71280aa04d2
wang xukun <wangxukun@hotmail.com>
2024-03-24 11:41:44 +0800

    core has bin and lib

--- core/cli/src/bin/index.ts
+++ core/cli/src/bin/index.ts
@@ -1,11 +1,19 @@
 #! /usr/bin/env node
 
+// 引入import-local库
 import importLocal from 'import-local';
 
-if (importLocal(__filename)) {
-  require('npmlog').info('cli', '正在使用wxkzd-cli本地版本');
-} else {
-  require('../lib')(process.argv.slice(2));
+// 检查当前的命令行工具是否有本地安装版本
+const isLocal = importLocal(__filename);
+console.log(isLocal);
+if (isLocal) {
+  // 如果有本地版本，import-local会自动引入本地版本并运行，然后结束当前进程
+  // 这样可以确保总是优先使用本地版本，而不是全局版本
   console.log(__filename);
-  require('npmlog').info('cli', importLocal(__filename));
+  console.log('Running local version');
+} else {
+  // 如果没有本地版本，就运行全局版本
+  console.log(__filename);
+  console.log('Running global version');
+  // 在这里添加你的全局版本代码...
 }
