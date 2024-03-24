d128c0f54d2cf1164b243dac0b36a0555ab4af8f
wang xukun <wangxukun@hotmail.com>
2024-03-24 13:35:51 +0800

    tsconfig.json target es6

--- core/cli/src/bin/index.ts
+++ core/cli/src/bin/index.ts
@@ -4,16 +4,12 @@
 import importLocal from 'import-local';
 
 // 检查当前的命令行工具是否有本地安装版本
-const isLocal = importLocal(__filename);
-console.log(isLocal);
-if (isLocal) {
+if (importLocal(__filename)) {
   // 如果有本地版本，import-local会自动引入本地版本并运行，然后结束当前进程
   // 这样可以确保总是优先使用本地版本，而不是全局版本
-  console.log(__filename);
   console.log('Running local version');
 } else {
   // 如果没有本地版本，就运行全局版本
-  console.log(__filename);
   console.log('Running global version');
   // 在这里添加你的全局版本代码...
 }
