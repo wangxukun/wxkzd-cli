de1922e4e443713a20dd123bb8d1e6df655ffcd7
wang xukun <wangxukun@hotmail.com>
2024-03-26 22:36:24 +0800

    checkRoot

---
+++ core/cli/src/lib/const.ts
@@ -0,0 +1,1 @@
+export const LOWEST_NODE_VERSION: string = '20.0.0';
--- core/cli/src/lib/index.ts
+++ core/cli/src/lib/index.ts
@@ -1,14 +1,38 @@
 module.exports = core;
 
+import semver from 'semver';
+import colors from 'colors/safe';
+import rootCheck from "root-check";
 // @ts-ignore
 import pkg from '../package.json';
 // @ts-ignore
 import log from '@wxkzd-cli/log';
+// @ts-ignore
+import {LOWEST_NODE_VERSION} from './const';
 
-function core() {
-    checkPkgVersion();
+function core(): void {
+    try {
+        checkPkgVersion();
+        checkNodeVersion();
+        checkRoot();
+
+    } catch (e: any) {
+        log.error("cli", e.message);
+    }
 }
 
-function checkPkgVersion() {
+function checkRoot() {
+    rootCheck();
+}
+
+function checkNodeVersion(): void {
+    const currentVersion: string = process.version;
+    const lowestVersion: string = LOWEST_NODE_VERSION;
+    if (!semver.gte(currentVersion, lowestVersion)) {
+        throw new Error(colors.red(`wxkzd-cli需要安装v${lowestVersion}以上版本的Node.js`));
+    }
+}
+
+function checkPkgVersion(): void {
     log.notice('cli', pkg.version);
 }
